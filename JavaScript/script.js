4e5503c5ef63827a261adfdb2c0230badfc633b4
mdsiaofficial <mdsiaofficial@gmail.com>
2023-04-19 11:26:11 +0600

    js let

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,6 @@
 let js = 'amazing';
-if (js==='amazing') alert ('JavaScript is FUN!');
+// this is used for comment
+// if (js==='amazing') alert ('JavaScript is FUN!');
 console.log(4+56+5);
 5 + 6 + 5;
 console.log('Ashiq');
@@ -12,3 +13,25 @@ firstName="Ashiq";
 console.log(firstName);
 let $name=3;
 console.log($name);
+let name='jannat';
+console.log(name);
+
+console.log('9'-'4');
+console.log(9-4);
+console.log('9-4');
+console.log("9-4");
+
+asif = 'asif';
+console.log(asif);
+
+let pi = 3.14159;
+console.log(pi);
+
+
+let country = 'Bangladesh';
+let continent = 'Asia';
+let population = '180 million';
+
+console.log(country);
+console.log(continent);
+console.log(population);
