958ee812d18a6d5538bf2cd8c265755f550909b6
mdsiaofficial <mdsiaofficial@gmail.com>
2023-04-28 00:36:54 +0600

    function done

--- JavaScript/script2.js
+++ JavaScript/script2.js
@@ -4,3 +4,11 @@ var hasDriversLicense = false;
 var passTest =  true;
 
 if(passTest) hasDriversLicense = true; console.log("Do it");
+
+// function
+
+function logger(){
+
+    console.log("Here!!!");
+}
+logger();
