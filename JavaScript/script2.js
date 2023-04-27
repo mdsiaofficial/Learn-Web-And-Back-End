75862ac51e1164c7ab21d69fb0a0b0ef2793337a
mdsiaofficial <mdsiaofficial@gmail.com>
2023-04-28 01:07:49 +0600

    js

--- JavaScript/script2.js
+++ JavaScript/script2.js
@@ -1,4 +1,4 @@
-//'use strict';
+'use strict';
 
 var hasDriversLicense = false;
 var passTest =  true;
@@ -12,3 +12,18 @@ function logger(){
     console.log("Here!!!");
 }
 logger();
+
+function greet(name){
+    var greet  = `Hi ${name}, How are you?`;
+    return greet;
+}
+console.log(greet("Ashiq"));
+
+function blender(apple, banana, orange){
+    var juice = `Making a juice with ${apple} apples, ${banana} bananas and ${orange} oranges.`;
+    return juice;
+}
+
+console.log(blender(5,3,3));
+
+
