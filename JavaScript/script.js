02fe59fc2af011bca478f5a487a6fbf91775401e
mdsiaofficial <mdsiaofficial@gmail.com>
2023-05-01 10:18:03 +0600

    writing in single js file

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -434,4 +434,107 @@ console.log(bill, tips);
 console.log(bill, tip);
 
 
+// Folder 3:
+'use strict';
 
+var hasDriversLicense = false;
+var passTest =  true;
+
+if(passTest) hasDriversLicense = true; console.log("Do it");
+
+// function
+
+function logger(){
+
+    console.log("Here!!!");
+}
+logger();
+
+function greet(name){
+    var greet  = `Hi ${name}, How are you?`;
+    return greet;
+}
+console.log(greet("Ashiq"));
+
+function blender(apple, banana, orange){
+    var juice = `Making a juice with ${apple} apples, ${banana} bananas and ${orange} oranges.`;
+    return juice;
+}
+
+console.log(blender(5,3,3));
+
+
+// LECTURE: Functions
+function describeCountry(country, population, capitalCity){
+    var output=`${country} has ${population} peopole and its capital city is ${capitalCity}.`;
+    return output;
+}
+console.log(describeCountry("Bangladesh", 18, "Dhaka"));
+
+
+// LECTURE: Function Declarations vs. Expressions
+var world_population = 7900; 
+var c = 18;
+function percentagOfWorld1(population){
+    return (population/world_population)*100;
+}
+var per1 = percentagOfWorld1(18);
+var per2 = percentagOfWorld1(180);
+var per3 = percentagOfWorld1(1492);
+
+console.log(`${c} million people, so it's about ${percentagOfWorld1(c)}% of 
+the world population.`);
+console.log(per1,per2,per3);
+
+
+// Arrow function
+
+var calcAge = birthYear => 2037 - birthYear;
+var ageCal =  calcAge(2002);
+console.log(ageCal);
+
+var newAge = (birthYear, firstName) => {
+    var ageNew = 2037 -birthYear;
+    var retire = 65-ageNew;
+    return `${firstName} retires in ${retire} years`;
+}
+console.log(newAge(2002, 'Ashiq'));
+console.log(newAge(2008, 'Jannat'));
+
+
+// LECTURE: Arrow Functions
+var percentagOfWorld3 = (population) => {
+    return (population/world_population)*100;
+}
+
+console.log(percentagOfWorld3(19));
+
+// Function calling
+function cutFruitPiece(fruit){
+    return  fruit*4;
+}
+function fruitProcess(apple, orange){
+    var applePieces = cutFruitPiece(apple);
+    var orangePieces = cutFruitPiece(orange);
+    var juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+    return juice;
+}
+console.log(fruitProcess(4,6));
+
+// LECTURE: Functions Calling Other Functions
+function describePopulation(country, population){
+    var out = `${country} has ${population} million people, which is about ${percentagOfWorld1(population)}% of the world.`;
+    return out;
+}
+console.log(describePopulation("Bangladesh", 180));
+console.log(describePopulation("China", 1441));
+console.log(describePopulation("Russia", 40));
+
+// Coding Challenge #1
+var checkWinner = (team1, team2) => {
+    if(team1 >= team2*2) console.log(`Dolphins win(${avrgDolphins} vs ${avrgKoalas}).`);
+    else if(team1*2 <= team2) console.log(`Koalas win(${avrgKoalas} vs ${avrgDolphins}).`);
+}
+// Test 1
+Dolphins1=44; Dolphins2=23; Dolphins3=71; Koalas1=65; Koalas2=54; Koalas3=49;
+checkWinner(Dolphins1, Koalas1);
