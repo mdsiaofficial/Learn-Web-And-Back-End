2c5e7f40296018d3c33f35dae1060101e982c728
mdsiaofficial <mdsiaofficial@gmail.com>
2023-05-12 10:54:56 +0600

    js

--- JavaScript/script2.js
+++ JavaScript/script2.js
@@ -1,4 +1,15 @@
 "use strict";
 
-const x=23;
-if(x===23) console.log(23);
+// problem
+
+const temps = [3,-4,5,14,34,-5,-10, 5,6,12,6,25,40];
+var min=temps[0];
+var max=temps[0];
+for(var i=0; i<temps.length; i++){
+    if(typeof(temps[i])!=='string'){
+        if(min<=temps[i]) min = temps[i];
+        if(max>=temps[i]) max = temps[i];
+    }
+    
+}
+console.log("Temperature amplitude: ", max-min);
