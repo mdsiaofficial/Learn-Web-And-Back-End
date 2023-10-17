785050fc243f2fdb4949784f000d3456c21c63e3
mdsiaofficial <mdsiaofficial@gmail.com>
2023-10-18 00:53:34 +0600

    a

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -15,13 +15,21 @@ console.log(document.querySelector('.guess').value);
 
 */
 
+const SecretNumber = Math.trunc(Math.random()*20)+1;
+document.querySelector('.number').textContent = SecretNumber;
+
 document.querySelector('.btncheck').addEventListener('click', function () {
     //console.log(document.querySelector('.guess').value);
-    //document.querySelector('.message').textContent = 'Correct Number! ✔';
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
-
+    
     if(!guess){
         document.querySelector('.message').textContent = 'No Number! ⛔';
+    }else if(guess === SecretNumber){
+        document.querySelector('.message').textContent = 'Correct Number! ✔';
+    }else if(guess> SecretNumber){
+        document.querySelector('.message').textContent = 'Too High ⏫';
+    }else if(guess<SecretNumber){
+        document.querySelector('.message').textContent = 'Too Low ⏬';
     }
 });
