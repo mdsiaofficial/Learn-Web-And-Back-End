524267a85c2a92e25529519659da27d2da567f24
mdsiaofficial <mdsiaofficial@gmail.com>
2023-10-20 17:20:29 +0600

    solved issue.

---
+++ ModalWindow/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// variables here:
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+
+// to show my all buttons:
+console.log(btnShowModal);
+
+
