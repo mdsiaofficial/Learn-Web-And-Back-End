e7ca98e633e5266fa2369d6c6d161ea23de0ba1d
mdsiaofficial <mdsiaofficial@gmail.com>
2023-10-20 23:20:44 +0600

    AAAA

--- ModalWindow/script.js
+++ ModalWindow/script.js
@@ -26,7 +26,21 @@ const showModal = function(){
 
 for(let i=0; i<btnShowModal.length; i++){
 
-    btnShowModal[i].addEventListener('click', showModal());
-    btnCloseModal.addEventListener('click', closeModal());
+    btnShowModal[i].addEventListener('click', function () {
+      console.log('Button Clicked');
+      // this button for clicking the modal button
+      modal.classList.remove('hidden');
+      // this will remove the 'hidden' string from 'modal hidden'
+      overlay.classList.remove('hidden'); // just for some styles background blur
+      //modal.style.display = 'block';
+    });
+	
+    btnCloseModal.addEventListener('click', function(){
+		modal.classList.add('hidden');
+    	overlay.classList.add('hidden');
+	});
 }
-overlay.addEventListener('click', closeModal ());
+overlay.addEventListener('click', function () {
+	modal.classList.add('hidden');
+	overlay.classList.add('hidden');
+});
