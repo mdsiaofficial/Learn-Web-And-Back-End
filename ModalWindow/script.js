28ce0f2dc2886d9e46182634d32cc7be7af33c73
mdsiaofficial <mdsiaofficial@gmail.com>
2023-10-20 17:09:25 +0600

    aa

--- ModalWindow/script.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelector('.show-modal');
-console.log(btnsOpenModal);
-console.log('name');
