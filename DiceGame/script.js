61f1a3c45e829084757b91c7a935f69a359eb1e0
mdsiaofficial <mdsiaofficial@gmail.com>
2023-10-22 01:00:42 +0600

    making dice game

--- DiceGame/script.js
+++ DiceGame/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// Selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+
+// Start Values:
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+
+// Rolling the dice:
+btnRoll.addEventListener('click', function(){
+    // 1. Generating a random dice roll here...
+    const dice = Math.trunc(Math.random()*6)+1;
+    console.log(dice);
+    
+
+    // 2. Now Remove hidden and Display dice:
+    diceEl.classList.remove('hidden');
+    diceEl.src = `${dice}.png`;
+})
