491da8528ca29aef2bc205bef91543b551cc48dc
mdsiaofficial <mdsiaofficial@gmail.com>
2023-10-26 01:22:45 +0600

    aa

--- JavaScript/script3.js
+++ JavaScript/script3.js
@@ -1,1 +1,18 @@
 'use strit';
+
+const name = "Ashiq";
+const first = function(){
+    let a=1;
+    const b = second(7,4);
+    a=a+b;
+    return a;
+}
+
+function second(x,y){
+    let c=3;
+    return c;
+}
+
+const x = first();
+
+console.log(x);
