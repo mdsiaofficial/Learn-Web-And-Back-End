e734d11393b51f91d7c180e6cd51edef883c0752
mdsiaofficial <mdsiaofficial@gmail.com>
2023-10-27 00:43:24 +0600

    aaa

--- JavaScript/script3.js
+++ JavaScript/script3.js
@@ -1,4 +1,4 @@
-'use strit';
+'use strict';
 
 const name = "Ashiq";
 const first = function(){
@@ -7,12 +7,67 @@ const first = function(){
     a=a+b;
     return a;
 }
-
 function second(x,y){
     let c=3;
     return c;
 }
-
 const x = first();
-
 console.log(x);
+
+function calcAge(birthYear){
+    let age = 2039-birthYear;
+    function printAge(){
+        const output = `Your age ${age}, born in ${birthYear}`;
+        console.log(firstName,age, output);
+
+        if(birthYear>=1981 && birthYear<=1996){
+            var s = true;
+            const firstName = "Lalu"
+            const str = `Oh, you are a millenial, ${firstName}`;
+            console.log(str);
+            function add(a,b){
+                return a+b;
+            }
+        }
+        //console.log(str);
+        console.log(s);    
+        //console.log(add(4,5));
+        //add(4,5);    
+    }
+    printAge();
+    return age;
+}
+const firstName = "Ashiq";
+calcAge(1992);
+//printAge();
+
+console.log(this);
+
+const clage = function(birthYear){
+    console.log(2037-birthYear);
+    console.log(this);
+}
+clage(1996);
+
+const clcage = birthYear => {
+    console.log(2037-birthYear);
+    console.log(this);
+}
+clcage(1996);
+
+const ashiq = {
+    year: 1999,
+    calage: function(){
+        console.log(this);
+    },
+}
+ashiq.calage();
+
+const mal = {
+    year:2015
+}
+mal.calage = ashiq.calage;
+mal.calage();
+
+const g=ashiq.calage;
+g();
