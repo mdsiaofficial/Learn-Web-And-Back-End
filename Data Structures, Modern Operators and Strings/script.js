7b1ce68f8c1609eef3af7675ae4dde49983fb8bc
mdsiaofficial <mdsiaofficial@gmail.com>
2023-10-29 00:15:48 +0600

    DSA Js

---
+++ Data Structures, Modern Operators and Strings/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const restaurant = {
+    name: 'Mayer Doa',
+    location: 'Rajashon, Savar, Dhaka',
+    categories: ['Bangali','Non-Vegetarian','Vegetarian','Organic'],
+    starterMenu: ['Ruti-Porota','Dim Vaji'],
+    mainMenu: ['Vat-Dal','Vat-Mas Vaji','Vat-Alu Vorta','Biriyani'],
+};
+
