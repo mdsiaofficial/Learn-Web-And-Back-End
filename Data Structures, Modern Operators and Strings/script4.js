8f46d50ee6965b7fdf60078035485d843a1ed969
mdsiaofficial <mdsiaofficial@gmail.com>
2023-11-26 10:10:04 +0600

    'uderscore_case' to 'camleCase'

--- Data Structures, Modern Operators and Strings/script4.js
+++ Data Structures, Modern Operators and Strings/script4.js
@@ -6,6 +6,8 @@
 //  
 
 document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement('p'));
+document.querySelector('p').innerText;
 document.body.append(document.createElement("button"));
 
 
@@ -14,11 +16,22 @@ document.querySelector('button').addEventListener('click', function () {
     console.log(txt);
     var rows = txt.split('\n');
     console.log(rows);
-    
+    newVars = [];
     for (var x of rows) {
+        console.log(x);
         var [n1, n2] = x.toLowerCase().trim().split('_');
+        console.log(n1, n2);
         n2 = n2[0].toUpperCase() + n2.slice(1);
         fullName = n1 + n2;
         console.log(fullName);
+        newVars.push(fullName);
     }
+    console.log(newVars);
+    for (var [i,x] of newVars.entries()) {
+        //console.log(i,x);
+        
+        console.log(`${x.padEnd(20)}${'✅'.repeat(i+1)}`);
+        
+    }
+    
 });
