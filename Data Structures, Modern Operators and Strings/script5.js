1b4ada623c6605547155875e56b41220c5013993
mdsiaofficial <mdsiaofficial@gmail.com>
2023-11-26 19:06:03 +0600

    more prac

---
+++ Data Structures, Modern Operators and Strings/script5.js
@@ -0,0 +1,7 @@
+var flight = '_Delayed_Departure;fao9344324;txl2134;11:23+_Arrival;Aru5323;fao9344324;11:56+_Delayed_Arrival;hel23443;fao9344324;12:40+_Departure;fao9344324;lis23423;12:55';
+
+// Delayed Departure from FAO to TXL (11h23m)
+//          Arrival from BRU to FAO (11h56m)
+// Delayed Arrival from HEL to FAO (12h40m)
+//          Departure from FAO to LIS (12h55m)
+
