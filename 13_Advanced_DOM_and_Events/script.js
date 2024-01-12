68ba6ee420b986b55f02f2ffdc08e415d2a26d97
mdsiaofficial <mdsiaofficial@gmail.com>
2024-01-12 13:11:36 +0600

    background random

--- 13_Advanced_DOM_and_Events/script.js
+++ 13_Advanced_DOM_and_Events/script.js
@@ -74,9 +74,29 @@ setTimeout(() => h1.removeEventListener("mouseenter", alertH1), 3000);
 
 
 
+// RGB (255,255,255)
 
+const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
+const randomColor = () => `rgb(${randomNum(0,255)},${randomNum(0,255)},${randomNum(0,255)})`;
 
+console.log(randomColor());
+
+document.querySelector(".nav__link").addEventListener("click", function (e) {
+	// console.log("links");
+	this.style.backgroundColor = randomColor();
+	
+});
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+	// console.log("links");
+	// this.style.backgroundColor = randomColor();
+	
+});
+document.querySelector(".nav").addEventListener("click", function (e) {
+	// console.log("links");
+	this.style.backgroundColor = randomColor();
+	
+});
 
 
 
