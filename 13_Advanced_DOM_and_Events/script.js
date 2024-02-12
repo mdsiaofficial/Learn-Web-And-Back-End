d2b731410b69e4670674491270656177b834ea2c
mdsiaofficial <mdsiaofficial@gmail.com>
2024-02-12 21:36:58 +0600

    added img loading when scrolling

--- 13_Advanced_DOM_and_Events/script.js
+++ 13_Advanced_DOM_and_Events/script.js
@@ -234,26 +234,29 @@ allSections.forEach(function (section) {
 });
 
 
-// lazy img //
+// lazy img  //
 const imgTargets = document.querySelectorAll("img[data-src]");
 // console.log(imgTargets);
 
 const loadImg = function (entries, observer) {
 	const [entry] = entries;
-	console.log(entry);
+	// console.log(entry);
 	
 	if (!entry.isIntersecting) return;
 	// replace  src with data-src:
 	entry.target.src = entry.target.dataset.src;
-	entry.target.classList.remove("lazy-img");
 	entry.target.addEventListener("load", function () {
+		entry.target.classList.remove("lazy-img");
 		
 	});
+
+	observer.unobserve(entry.target);
 };
 
 const imgObserver = new IntersectionObserver(loadImg, {
 	root: null,
 	threshold: 0,
+	rootMargin: +"200px",
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));
