e9f6b525b6abb20b7597c9894d953d1e0c7c3380
mdsiaofficial <mdsiaofficial@gmail.com>
2024-02-15 12:10:36 +0600

    Learning OOP in Javascript

--- 14_Object_Oriented_Programming_OOP_With_JavaScript/script.js
+++ 14_Object_Oriented_Programming_OOP_With_JavaScript/script.js
@@ -7,10 +7,24 @@ const Person = function(firstName, birthYear){
     // Assign the birthYear argument to the birth_Year property of the new object
     this.birth_Year = birthYear;
 
+        // never do this.
+    // this.calcAge = function(){
+    //     const currentYear = new Date().getFullYear();
+    //     return currentYear - this.birth_Year;
+    //     // console.log(currentYear - this.birth_Year);
+    // }
 }
 
+// Ashiq here is a instance of Person
 const Ashiq = new Person("Ashiq", 2000);
 console.log(Ashiq);
+// Ashiq here is a instance of Person : True
+console.log(Ashiq instanceof Person); 
+
+// but here:
+const Jannat = "Jannat";
+// Jannat here is not a instance of Person : True
+console.log(Jannat instanceof Person);
 
 // 1. New{} is created
 // 2. function is called, this ={}
@@ -22,3 +36,29 @@ console.log(Taher);
 
 const Rony  = new Person("Rony", 2000);
 console.log(Rony);
+
+
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function(){
+    const currentYear = new Date().getFullYear();
+    // return currentYear - this.birth_Year;
+    console.log(currentYear - this.birth_Year);
+
+}
+
+Ashiq.calcAge();
+Taher.calcAge();
+
+console.log(Ashiq.__proto__);
+console.log(Ashiq.__proto__ ===Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(Ashiq));
+console.log(Person.prototype.isPrototypeOf(Rony));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototypeOfLinkedObjects
+
+Person.prototype.species = "Homo Sapiens";
+console.log(Ashiq.species);
